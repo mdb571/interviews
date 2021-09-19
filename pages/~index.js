ce081d401b6a333128e2d1d9d3f4c9b1f65e763b
mdbilal <rmb571@rmb571-debian>
2021-09-19 15:48:55 +0530

    initial commit

--- pages/~index.js
+++ pages/~index.js
@@ -1,35 +1,41 @@
+import Nav from "../components/nav";
 import { getWhyNextReasons } from "../lib/api";
 
 export default function IndexPage({ reasons }) {
   return (
     <div>
+      <Nav />
       <div className="container mx-auto py-20 px-8">
         <h1 className="text-5xl text-center text-accent-1 mb-16">
-          Interview Exp EC`22
+          Interview Experience EC`22
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {reasons
-            .slice(0, reasons.length)
-            .map(({ name, company, technical,hr,suggestion,status }) => (
+            .slice(0, reasons.length - 1)
+            .map(({ name, company, position}) => (
               <a
                 className="border border-grey-200 rounded p-4 hover:shadow-lg hover:border-transparent"
+                href="#"
               >
-                <h3 className="font-bold mb-2">{name} ({company} {status})</h3>
-                <h4 className="fon-bold mb-3"> Technical Questions </h4>
-                <div className="mb-3" dangerouslySetInnerHTML={{ __html: technical }} />
-               
-                <h4 className="fon-bold mb-3"> HR Questions </h4>
-                <div className="mb-3" dangerouslySetInnerHTML={{ __html: hr }} />
-                
-                 <h4 className="fon-bold mb-3"> Suggestions </h4>
-                <div className="mb-3" dangerouslySetInnerHTML={{ __html: suggestion }} />
-                
+                <h3 className="font-bold mb-2">{name}</h3>
+                <div dangerouslySetInnerHTML={{ __html: company }} />
+                <span className="mt-4 block">
+
+               </span>
               </a>
             ))}
         </div>
-     </div>
-  </div>
+        <div className="text-center mt-8">
+          {reasons.slice(reasons.length - 1).map(({ name, company }) => (
+            <div className="markdown inline-p">
+              <strong>{name}</strong>{" "}
+              <span dangerouslySetInnerHTML={{ __html: company }} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
